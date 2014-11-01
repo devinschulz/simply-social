d4e6eab379bcb2c6d4610a3b61147e8248cb1a92
Devin Schulz <devin@devinschulz.com>
2014-10-31 22:08:04 -0400

    Add Modal

---
+++ app/modal/modal.js
@@ -0,0 +1,43 @@
+/**
+ * Modal Controller Logic.
+ *
+ * This controller is triggered from the header controller.
+ *
+ * @param $scope
+ * @param $timeout
+ * @param $rootScope
+ * @param close Method imported through angularModalService
+ */
+function ModalCtrl($scope, $timeout, $rootScope, close, $document) {
+
+  function toggleModalState() {
+    $scope.open = $scope.open ? false : true;
+  }
+
+  $timeout(toggleModalState, 200);
+
+  function triggerClose() {
+    $rootScope.modalOpen = false;
+    toggleModalState();
+    close({});
+  }
+
+  $scope.closeModal = function () {
+    triggerClose();
+  };
+
+  function keyUpHandler(keyEvent) {
+    if (keyEvent.keyCode == 27) { // Esc
+      triggerClose();
+    }
+  }
+
+  $document.on('keyup', keyUpHandler);
+  $scope.$on('$destroy', function() {
+    $document.off('keyup', keyUpHandler)
+  });
+}
+
+angular
+  .module('app')
+  .controller('ModalCtrl', ModalCtrl);
